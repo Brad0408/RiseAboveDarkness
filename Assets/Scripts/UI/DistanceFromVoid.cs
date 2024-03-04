7a6674d7dcb19aa499de007899f9d1e44d402f61
Brad <92309314+Brad0408@users.noreply.github.com>
2024-03-04 22:59:42 +0000

    Update DistanceFromVoid.cs

--- Assets/Scripts/UI/DistanceFromVoid.cs
+++ Assets/Scripts/UI/DistanceFromVoid.cs
@@ -15,24 +15,28 @@ public class DistanceFromVoid : MonoBehaviour
     
     private void Update()
     {
+        //Finds distance between player and the the rising darkness
         distance = (darkvoid.transform.position - transform.position).magnitude;
 
-        //SO DUMB THIS SHOWS WHEN LOWER THAN 25, NOT BUGGED (-25 MINS OF MY LIFE) THIS IS WHY U ADD COMMENTS WHEN YOU MAKE SOMETHING
+        //When less than 25 render text on screen
         if (distance <= 25.0f)
         {
             TextDistance.text = "Void Distance: " + distance.ToString("F1") + "m";
         }
         else
         {
+            //dont render is greater
             TextDistance.text = null;
         }
 
+        //If its at 10 make the text red
         if (distance <= 10.0f)
         {
             TextDistance.color = Color.red;
         }
         else
-        {
+        {    
+            //Keep text white
             TextDistance.color = Color.white;
         }
     }
