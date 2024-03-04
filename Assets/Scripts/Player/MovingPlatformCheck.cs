bb56c971134c985bb0b81925b325d149c84325cd
Brad <92309314+Brad0408@users.noreply.github.com>
2024-03-04 22:40:06 +0000

    Update MovingPlatformCheck.cs

--- Assets/Scripts/Player/MovingPlatformCheck.cs
+++ Assets/Scripts/Player/MovingPlatformCheck.cs
@@ -10,6 +10,8 @@ public class MovingPlatformCheck : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("MovingPlatform"))
         {
+            //If the player is on moving platform parent player to the moving platform to move them along with it
+            
             Debug.Log("On Moving Platform");
             player.transform.parent = collision.gameObject.transform;
         }
@@ -17,6 +19,9 @@ public class MovingPlatformCheck : MonoBehaviour
 
     private void OnTriggerExit2D(Collider2D collision)
     {
+        //Remove parenting when not standing on platform
+
+        
         if (collision.gameObject.CompareTag("MovingPlatform"))
         {
             player.transform.parent = null;
