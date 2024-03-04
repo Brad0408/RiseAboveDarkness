b362a706843a73da5deadcdd62c72e8146665c13
Brad <92309314+Brad0408@users.noreply.github.com>
2024-03-04 22:48:25 +0000

    Update PlaySoundOnClick.cs

--- Assets/Scripts/Sound/PlaySoundOnClick.cs
+++ Assets/Scripts/Sound/PlaySoundOnClick.cs
@@ -6,7 +6,7 @@ public class PlaySoundOnClick : MonoBehaviour
 {
     [SerializeField] private AudioClip clip;
 
-    //Really flawed sound sound system making use of singleton instances... yeah. If two sounds happen at once the latter one overides the first sound
+    //Really flawed sound sound system making use of singleton instances... yeah. If two sounds happen at once the latter one overides the first sound. Works just enough for this project size
 
     //Can be called to play sound effect
     public void PlaySoundEffect()
