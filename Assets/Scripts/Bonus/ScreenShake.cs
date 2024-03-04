acb6b0fc5de254ef74fe391187784e9f5daed3ca
Brad <92309314+Brad0408@users.noreply.github.com>
2024-03-04 22:01:07 +0000

    Update ScreenShake.cs

--- Assets/Scripts/Bonus/ScreenShake.cs
+++ Assets/Scripts/Bonus/ScreenShake.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class ScreenShake : MonoBehaviour
 {
+    //Script to casue screen shake
     public IEnumerator Shake(float duration, float magnitude)
 
     { 
@@ -11,19 +12,21 @@ public class ScreenShake : MonoBehaviour
 
         float elapsed = 0.0f;
 
+        //Effect the screen
         while (elapsed < duration) 
         {
             float x = Random.Range(-1f, 1f) * magnitude;
             float y = Random.Range(-1f, 1f) * magnitude;
 
-
+            //Make the screen shake
             transform.localPosition = new Vector3 (x, y, originalPosition.z);
 
             elapsed += Time.deltaTime;
 
             yield return null;
         }
-        
+
+        //Set back to normal
         transform.localPosition = originalPosition;
 
     }
