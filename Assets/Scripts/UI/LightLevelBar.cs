3ef02255a4e67973dad09b8d465d1454cadf498c
Brad <92309314+Brad0408@users.noreply.github.com>
2024-03-04 23:03:14 +0000

    Update LightLevelBar.cs

--- Assets/Scripts/UI/LightLevelBar.cs
+++ Assets/Scripts/UI/LightLevelBar.cs
@@ -8,10 +8,11 @@ using UnityEngine.UI;
 public class LightLevelBar : MonoBehaviour
 {
     [SerializeField] private Image lightbar;
-    [SerializeField] private Light2D playerLight;
+    [SerializeField] private Light2D playerLight;  //Access players light level
 
     private float percentage;
 
+    //Fill the UI bar as much as the light intensity is equal to
     private void Update()
     {
         percentage = (playerLight.intensity/20.0f);
