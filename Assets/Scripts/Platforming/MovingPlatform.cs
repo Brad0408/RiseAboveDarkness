d3a313bd69eb7e7cf2470c0b5c4a043434205c9b
Brad <92309314+Brad0408@users.noreply.github.com>
2024-03-04 22:31:25 +0000

    Update MovingPlatform.cs

--- Assets/Scripts/Platforming/MovingPlatform.cs
+++ Assets/Scripts/Platforming/MovingPlatform.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class MovingPlatform : MonoBehaviour
 {
+    //Make accessible to editor
     [SerializeField] private Transform Loc1, Loc2;
     [SerializeField] private float speed;
     [SerializeField] private Transform startPos;
@@ -16,8 +17,10 @@ public class MovingPlatform : MonoBehaviour
         nextPos = startPos.position;
     }
 
+    //Make the platform move
     private void Update()
     {
+        //Aleternaet destination positions
         if (transform.position == Loc1.position)
         {
             nextPos = Loc2.position;
@@ -27,6 +30,7 @@ public class MovingPlatform : MonoBehaviour
             nextPos = Loc1.position;
         }
 
+        //When it reaches a postion set new next position and travel towards it
         transform.position = Vector3.MoveTowards(transform.position, nextPos, speed * Time.deltaTime);
 
     }
